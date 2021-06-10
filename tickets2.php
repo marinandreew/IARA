42ec0fff6b85a446598956524fed038faed1fb15
Firotss <77095260+Firotss@users.noreply.github.com>
2021-06-11 02:28:19 +0300

    Add files via upload

---
+++ tickets2.php
@@ -0,0 +1,18 @@
+
+<link rel="stylesheet" href="../css/tickers2.css">
+
+<div class="flipcard h">
+    <div class="front">
+      <div class="rectangle-four">
+        <h1>GOLD TICKET</h1>
+      </div>
+    </div>
+    <div class="back">
+      <div class="rectangle-four">
+        <h1>GOLD TICKET</h1>
+      </div>
+    </div>
+</div>
+
+
+
